afef71960267eeec2a50909b52c213f11c1dd33a
Aitor <asantin@indaba.lks.es>
2021-09-07 15:46:24 +0200

    Crear componente RegistrarUsuario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { ThemeProvider } from '@material-ui/core'
 import React from "react";
-import Login from "./componentes/seguridad/Login";
+import RegistrarUsuario from './componentes/seguridad/RegistrarUsuario';
 import theme from './theme/theme';
 
 
 function App() {
   return (
       <ThemeProvider theme={theme} >
-        <Login />
+        <RegistrarUsuario />
       </ThemeProvider>
       
   );
---
+++ src/componentes/seguridad/RegistrarUsuario.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Container, Grid, Card, Typography, Avatar, Icon, TextField, Button, Link    } from '@material-ui/core';
+import UseStyles from '../../theme/UseStyles';
+
+const RegistrarUsuario = () => {
+    const classes = UseStyles();
+    return (
+        <Container className={classes.containermt}>
+            <Grid container justify="center">
+                <Grid item lg={6} md={8}>
+                    <Card className={classes.card} align="center" >
+                        <Avatar className={classes.avatar}>
+                            <Icon className={classes.icon}>
+                                person_add
+                            </Icon>
+                        </Avatar>
+                        <Typography variant="h5" color="primary">
+                            Registro de usuario
+                        </Typography>
+                        <form className={classes.form}>
+                            <Grid container spacing={2}>
+                                <Grid item md={6} xs={12} className={classes.gridmb} >
+                                    <TextField label="nombre" name="nombre" variant="outlined" fullWidth />
+                                </Grid>
+                                <Grid item md={6} xs={12} className={classes.gridmb} >
+                                    <TextField label="Apellidos" name="apellido" variant="outlined" fullWidth />
+                                </Grid>
+                                <Grid item md={12} xs={12} className={classes.gridmb} >
+                                    <TextField label="Username" name="username" variant="outlined" fullWidth />
+                                </Grid>
+                                <Grid item md={12} xs={12} className={classes.gridmb} >
+                                    <TextField label="Email" name="email" type="email" variant="outlined" fullWidth />
+                                </Grid>
+                                <Grid item md={12} xs={12} className={classes.gridmb} >
+                                    <TextField label="Password" name="password" type="password" variant="outlined" fullWidth />
+                                </Grid>
+                                <Grid item md={12} xs={12} className={classes.gridmb} >
+                                    <Button variant="contained" fullWidth color="primary" type="submit" >
+                                        Registrar
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <Link to="/login" variant="body1" className={classes.link}>
+                                ¿Ya tienes una cuenta? Inicia Sesion
+                            </Link>
+                        </form>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default RegistrarUsuario;
