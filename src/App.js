c639b7cc9be8034c34b1ebefd1e567d0af5c2850
Aitor <asantin@indaba.lks.es>
2021-09-07 09:26:08 +0200

    Dejamos de nuevo como estaba app.js para empezar

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Bienvenidos al curso de ASP.NET Core y React Hooks</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-    
   );
 }
 
