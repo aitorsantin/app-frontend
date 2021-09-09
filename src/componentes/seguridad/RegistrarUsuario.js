9db81d17a892c8a922bd303fd006b705a2de1ef3
Aitor <asantin@indaba.lks.es>
2021-09-09 09:42:29 +0200

    Configurar registrar usuario para introducir datos

--- src/componentes/seguridad/RegistrarUsuario.js
+++ src/componentes/seguridad/RegistrarUsuario.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Grid, Card, Typography, Avatar, Icon, TextField, Button    } from '@material-ui/core';
 import UseStyles from '../../theme/UseStyles';
 import { Link } from 'react-router-dom';
 
+const clearUsuario = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    password: '',
+    username: ''
+}
+
 const RegistrarUsuario = () => {
+    const [ usuario, setUsuario ] = useState({
+        nombre: '',
+        apellido: '',
+        email: '',
+        password: '',
+        username: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setUsuario(prev => ({
+            ...prev,
+            [name]: value
+        }))
+    };
+
+    const guardarUsuario = () => {
+        //const insertarData = nuevoUsuario(usuario);
+        console.log("Mi usuario es", usuario);
+        setUsuario(clearUsuario);
+        /*registrarUsuario(usuario).then(response => {
+            console.log('el objeto response que envia el servidor', response);
+            //Guardamos el valor del parametro token.
+            window.localStorage.setItem('token', response.data.token);
+        });*/
+
+    }
+
     const classes = UseStyles();
     return (
         <Container className={classes.containermt}>
@@ -18,25 +54,25 @@ const RegistrarUsuario = () => {
                         <Typography variant="h5" color="primary">
                             Registro de usuario
                         </Typography>
-                        <form className={classes.form}>
+                        <form className={classes.form} onSubmit={(e) => e.preventDefault()} >
                             <Grid container spacing={2}>
                                 <Grid item md={6} xs={12} className={classes.gridmb} >
-                                    <TextField label="nombre" name="nombre" variant="outlined" fullWidth />
+                                    <TextField label="nombre" name="nombre" value={usuario.nombre} onChange={handleChange} variant="outlined" fullWidth />
                                 </Grid>
                                 <Grid item md={6} xs={12} className={classes.gridmb} >
-                                    <TextField label="Apellidos" name="apellido" variant="outlined" fullWidth />
+                                    <TextField label="Apellidos" name="apellido" value={usuario.apellido} onChange={handleChange} variant="outlined" fullWidth />
                                 </Grid>
                                 <Grid item md={12} xs={12} className={classes.gridmb} >
-                                    <TextField label="Username" name="username" variant="outlined" fullWidth />
+                                    <TextField label="Username" name="username" value={usuario.username} onChange={handleChange} variant="outlined" fullWidth />
                                 </Grid>
                                 <Grid item md={12} xs={12} className={classes.gridmb} >
-                                    <TextField label="Email" name="email" type="email" variant="outlined" fullWidth />
+                                    <TextField label="Email" name="email" value={usuario.email} onChange={handleChange} type="email" variant="outlined" fullWidth />
                                 </Grid>
                                 <Grid item md={12} xs={12} className={classes.gridmb} >
-                                    <TextField label="Password" name="password" type="password" variant="outlined" fullWidth />
+                                    <TextField label="Password" name="password" value={usuario.password} onChange={handleChange} type="password" variant="outlined" fullWidth />
                                 </Grid>
                                 <Grid item md={12} xs={12} className={classes.gridmb} >
-                                    <Button variant="contained" fullWidth color="primary" type="submit" >
+                                    <Button variant="contained" fullWidth color="primary" type="submit" onClick={guardarUsuario} >
                                         Registrar
                                     </Button>
                                 </Grid>
