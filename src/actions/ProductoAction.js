8d7540f2bccccc9d6309949691f6e824db778d7e
Aitor <asantin@indaba.lks.es>
2021-09-13 08:46:18 +0200

    Crear los modulos para realizar las llamadas a la Api y instalar axios

---
+++ src/actions/ProductoAction.js
@@ -0,0 +1,24 @@
+import HttpCliente from '../servicios/HttpCliente';
+
+export const getProductos = (request) =>
+{
+    /*Las promesas sirven para esperar la respuesta del servidor antes de mostrar la pagina*/
+    return new Promise( (resolve, eject) => {
+        HttpCliente.get(`/api/productos/listproductosspec?pageIndex=${request.oageIndex}
+        &pageSize=${request.pageSize}&search=${request.search}`).then(response => {
+            resolve(response);
+        });
+    })
+};
+
+/*
+    response 
+        count : 5
+        pageIndex: 1
+        pageSize: 5
+        pageCount: 3
+        data: [
+            {....},
+            {....}
+        ]
+*/
