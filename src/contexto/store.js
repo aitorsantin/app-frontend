9e8d79a6ef1d79190796dd57166e75f56f7b8a37
Aitor <asantin@indaba.lks.es>
2021-09-15 15:57:15 +0200

    Empezando a crear variables globales con Redux

---
+++ src/contexto/store.js
@@ -0,0 +1,16 @@
+import React, {createContext, useContext, useReducer} from 'react'; 
+
+//este objeto va a almacenar las variables de estado
+export const stateContext = createContext();
+
+//initialState variable global a la que queremos tener acceso
+
+export const StateProvider = ( { reducer, initialState, children } ) => (
+    //Todo los componentes podran acceder al a todo lo que entregue el StateContext.Provider
+    //reducer es la funcion que permite modificar la variable
+    <stateContext.Provider value={useReducer(reducer, initialState)}>
+        {children}
+    </stateContext.Provider>
+);
+
+export const useStateValue = () => useContext(stateContext); 
