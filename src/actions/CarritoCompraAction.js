0dc3ce7fd56fa68638a66bca2c53891f3fd5f818
Aitor <asantin@indaba.lks.es>
2021-09-24 10:56:10 +0200

    resolver bug CarritoCompraAction

--- src/actions/CarritoCompraAction.js
+++ src/actions/CarritoCompraAction.js
@@ -6,18 +6,21 @@ instancia.CancelToken = axios.CancelToken;
 instancia.isCancel = axios.isCancel;    
 
 export const getCarritoCompra = (dispatch, id) => {
-    return new Promise( (ressolve, eject) => {
+    return new Promise( (resolve, eject) => {
+
         instancia.get(`/api/carritocompra?id=${id}`)
         .then(response => {
+
             dispatch({
-                type="CARRITO_SESION",
-                id= response.data.id,
-                items = response.data.items
+                type: "CARRITO_SESION",
+                id: response.data.id,
+                items: response.data.items
             });
-            ressolve(response)
+            resolve(response);
         })
-        .catch(error =>{
-            ressolve(error.response);
+        .catch(error => {
+            resolve(error.response);
         });
-    }); 
+    });
+
 }
